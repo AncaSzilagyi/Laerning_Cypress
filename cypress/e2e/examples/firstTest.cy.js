2d2789fc022682522ea2cdc9af4362b8211c5338
elisa.szilagyi <elisa.szilagyi@altom.com>
2022-12-17 12:28:11 +0200

    Modified tests structure

--- cypress/e2e/examples/firstTest.cy.js
+++ cypress/e2e/examples/firstTest.cy.js
@@ -17,17 +17,89 @@ describe('Elements tests', () => {
         cy.get("#currentAddress").type("This is the current address.");
         cy.get("#permanentAddress").type("This is my permanent address.");
         cy.get("#submit").click();
+
+        //todo assert that all the tasks appear under the form
+        //todo try to input an invalid email
     })
 
-    //todo assert that all the tasks appear under the form
-    //todo try to input an invalid email
+    it('Check Box', () => {
+        cy.visit("https://demoqa.com/"); // Opens the URL
+        cy.xpath('//div/h5[contains(text(), "Elements")]').click();
+        cy.xpath("//span[contains(text(), 'Check Box')]").click();
+
+        cy.xpath("//button[@aria-label='Expand all']").click({ force: true });
+        cy.xpath("//span[contains(text(),'Notes')]/../span[1]").click();
+
+        cy.checkbox_class_assert('Notes', 'check');
+        cy.checkbox_class_assert('Desktop', 'half-check');
+        cy.checkbox_class_assert('Home', 'half-check');
+
+        cy.xpath("//span[contains(text(),'Desktop')]/../span[1]").click();
+
+        cy.checkbox_class_assert('Notes', 'check');
+        cy.checkbox_class_assert('Desktop', 'check');
+        cy.checkbox_class_assert('Commands', 'check');
+
+        cy.xpath("//span[contains(text(),'Home')]/../span[1]").click();
+        cy.xpath("//span[contains(text(),'Home')]/../span[1]").click();
+
+        cy.checkbox_class_assert('Desktop', 'uncheck');
+        cy.checkbox_class_assert('Documents', 'uncheck');
+        cy.checkbox_class_assert('Downloads', 'uncheck');
+
+    })
 
     it('Radio button', () => {
         cy.visit("https://demoqa.com/"); // Opens the URL
         cy.xpath('//div/h5[contains(text(), "Elements")]').click();
         cy.xpath("//span[contains(text(), 'Radio Button')]").click();
-        cy.xpath("//label[contains(text(),'Yes')]/../input").click({force:true}).should('be.checked');
+        cy.xpath("//label[contains(text(),'Yes')]/../input").click({ force: true }).should('be.checked');
     })
+
+    it('Web Tables', () => {
+        cy.visit("https://demoqa.com/"); // Opens the URL
+        cy
+            .xpath('//div/h5[contains(text(), "Elements")]')
+            .click();
+        cy
+            .xpath("//span[contains(text(), 'Web Tables')]")
+            .click();
+
+        cy
+            .xpath('//button[contains(text(),"Add")]')
+            .click();
+        cy
+            .xpath("//input[@placeholder='First Name']")
+            .type('Elisa');
+        cy
+            .xpath("//input[@placeholder='Last Name']")
+            .type('Enea');
+        cy
+            .xpath("//input[@placeholder='name@example.com']")
+            .type('test@gmail.com');
+        cy
+            .xpath("//input[@placeholder='Age']")
+            .type('23');
+        cy
+            .xpath("//input[@placeholder='Salary']")
+            .type('4000');
+        cy
+            .xpath("//input[@placeholder='Department']")
+            .type('testing department');
+        cy
+            .xpath("//button[@id='submit']")
+            .click();
+
+        // to validate here some invalid values or empty fields.
+
+        cy
+            .xpath("//input[@placeholder='Type to search']")
+            .type('Elisa');
+        cy
+            .xpath("//div[@class='rt-tr -odd']/div[1]")
+            .should('have.text', 'Elisa');
+    })
+
 
     it('Buttons', () => {
         cy.visit("https://demoqa.com/"); // Opens the URL
@@ -42,47 +114,5 @@ describe('Elements tests', () => {
         cy.get('#rightClickMessage').should('contain.text', 'You have done a right click');
         cy.get('#dynamicClickMessage').should('contain.text', 'You have done a dynamic click');
     })
-
-    it('Buttons', () => {
-        cy.visit("https://demoqa.com/"); // Opens the URL
-        cy.xpath('//div/h5[contains(text(), "Elements")]').click();
-        cy.xpath("//span[contains(text(), 'Buttons')]").click();
-        cy.xpath("//button[@id='doubleClickBtn']").dblclick();
-        cy.xpath("//button[@id='rightClickBtn']").rightclick();
-        cy.xpath("//button[text()='Click Me']").click();
-
-        cy.get('#doubleClickMessage').should('contain.text', 'You have done a double click');
-        cy.get('#rightClickMessage').should('contain.text', 'You have done a right click');
-        cy.get('#dynamicClickMessage').should('contain.text', 'You have done a dynamic click');
-    })
-    
-    it('Check Box', () => {
-        cy.visit("https://demoqa.com/"); // Opens the URL
-        cy.xpath('//div/h5[contains(text(), "Elements")]').click();
-        cy.xpath("//span[contains(text(), 'Check Box')]").click();
-
-        cy.xpath("//button[@aria-label='Expand all']").click({force:true});
-        cy.xpath("//span[contains(text(),'Notes')]/../span[1]").click(); 
-        
-        cy.checkbox_class_assert('Notes', 'check');
-        cy.checkbox_class_assert('Desktop', 'half-check');
-        cy.checkbox_class_assert('Home', 'half-check');
-
-        cy.xpath("//span[contains(text(),'Desktop')]/../span[1]").click(); 
-        
-        cy.checkbox_class_assert('Notes', 'check');
-        cy.checkbox_class_assert('Desktop', 'check');
-        cy.checkbox_class_assert('Commands', 'check');
-
-        cy.xpath("//span[contains(text(),'Home')]/../span[1]").click(); 
-        cy.xpath("//span[contains(text(),'Home')]/../span[1]").click(); 
-
-        cy.checkbox_class_assert('Desktop', 'uncheck');
-        cy.checkbox_class_assert('Documents', 'uncheck');
-        cy.checkbox_class_assert('Downloads', 'uncheck');
-        
-    })
-
-
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -28,5 +28,10 @@ Cypress.Commands.add('checkbox_class_assert', (label_name, status) => {
     var xpath_name = "//span[contains(text(),'" + label_name + "')]/../span[1]/*[local-name()='svg']";
     var class_name = 'rct-icon rct-icon-' + status;
     cy.xpath(xpath_name).should('have.class', class_name);
-
 })
+
+// Cypress.Commands.add('check_table_field_value', (field_column_no, value) => {
+//     var xpath_field = "//div[@class='rt-tr -odd']/div["+field_column_no+"]";
+//     cy.xpath(xpath_field).should('have.text', value);
+// })
+// I want to create a function that is automatically checking the entire row from the table. Work in progres...

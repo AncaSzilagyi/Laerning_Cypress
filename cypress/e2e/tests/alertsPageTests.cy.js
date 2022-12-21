098cc60e7194a49487b9c9a6cc581a7634f630f1
elisa.szilagyi <elisa.szilagyi@altom.com>
2022-12-21 12:06:58 +0200

    Edited alarm tests

--- cypress/e2e/tests/alertsPageTests.cy.js
+++ cypress/e2e/tests/alertsPageTests.cy.js
@@ -8,12 +8,42 @@ describe('Alerts tests', () => {
         cy.xpath("//div[@class='main-header']").should('have.text', 'Alerts');
     })
 
-    it('Click and assert', () => {
+    it('Alert button', () => {
 
         cy.xpath("//button[@id='alertButton']").click();
-        cy.on('window:alert',(t)=>{
+        cy.on('window:alert', (t) => {
             expect(t).to.contains('You clicked a button');
-         })
+        })
     })
+
+    it('Timer alert button', () => {
+        cy.xpath("//button[@id='timerAlertButton']").click();
+        cy.wait(6000);
+        cy.on('window:alert', (t) => {
+            expect(t).to.contains('This alert appeared after 5 seconds');
+        })
+    })
+
+    it('Confirm alert button', () => {
+        cy.xpath("//button[@id='confirmButton']").click();
+        cy.on('window:alert', (t) => {
+            expect(t).to.contains('You clicked a button');
+        })
+        cy.on('window:confirm', () => true)
+        cy.get('#confirmResult').should('have.text', 'You selected Ok');
+    })
+
+    // it.only('Confirm alert button', () => {
+    //     cy.xpath("//button[@id='promtButton']").click();
+    //     cy.window().then(function($win){
+    //         cy.stub($win, 'prompt').returns('Elisa');
+    //     })
+    //     cy.on('window:confirm', () => true)
+    //     cy.get('#promptResult').should('have.text', 'You entered Elisa');
+
+    // })
 })
+
+
+
 
